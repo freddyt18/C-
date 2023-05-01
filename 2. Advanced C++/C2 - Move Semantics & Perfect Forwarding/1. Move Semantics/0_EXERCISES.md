0751623b6d6d5b05787566a3e1c67aaee02a1473
freddyt18 <keopitou.doung@student.cadt.edu.kh>
2023-05-02 05:22:48 +0700

    Exercise 3, Section 1 of Chapter 2 of Book AV_C++

--- 2. Advanced C++/C2 - Move Semantics & Perfect Forwarding/1. Move Semantics/0_EXERCISES.md
+++ 2. Advanced C++/C2 - Move Semantics & Perfect Forwarding/1. Move Semantics/0_EXERCISES.md
@@ -4,8 +4,4 @@ Some exercises:
 
 2. Vector Move: Create a vector of objects with a move constructor and move assignment operator. Use it to move elements from one index to another and verify that the move constructor is called and the original element is left in a valid but unspecified state.
 
-3. Function Move: Create a function that takes a move-only object as a parameter and returns a moved object. Use it to move an object and verify that the move constructor is called and the original object is left in a valid but unspecified state.
-
-4. Custom Type Move: Create a custom class with a move constructor and move assignment operator that uses a resource (e.g. a dynamic memory allocation). Use it to move an object and verify that the resource is properly transferred and the original object is left in a valid but unspecified state.
-
-5. Copy vs Move: Create a program that copies and moves objects of different sizes and measures the time it takes. Compare the performance of copying and moving for different types of objects and sizes.
+3. Copy vs Move: Create a program that copies and moves objects of different sizes and measures the time it takes. Compare the performance of copying and moving for different types of objects and sizes.
---
+++ 2. Advanced C++/C2 - Move Semantics & Perfect Forwarding/1. Move Semantics/4_move_vs_copy.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <map>
+#include <thread>
+
+class School {
+    public:
+        School(){
+            std::string m = "";
+            for(int x=0; x<10; x++){
+                for(int i=0; i<10; i++){
+                    for(int j=0; j<10; j++){
+                        m += "Major";
+                    }
+                    majors.push_back(m + std::to_string(i));
+                }
+            }
+
+            for(int i=0; i<10; i++){
+                category.push_back(majors);
+            }
+
+            // std::cout << "Default Constructor" << std::endl;
+        }
+
+        School(const School& other){
+            
+            this->category = other.category;
+            this->majors = other.majors;
+
+            // std::cout << "Copy Constructor" << std::endl;
+        }
+
+        School(School&& other){
+
+            this->category = std::move(other.category);
+            this->majors = std::move(other.majors);
+
+            // std::cout << "Move Constructor" << std::endl;
+        }
+
+        // ~School(){
+        //     std::cout << "Destructor" << std::endl;
+        // }
+
+    private:
+        std::vector<std::string> majors;
+        std::vector<std::vector<std::string>> category;
+};
+
+std::map<int, std::vector<int>> copy_move_value;
+std::vector<std::thread> tr;
+int x = 3000;
+
+void worker(int T){
+    while(x > 0){
+        // std::cout << "T" << T <<" - Iteration: " << x << std::endl;
+
+        int copy;
+        int move;
+
+        auto start = std::chrono::high_resolution_clock::now();
+        School a{};
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // std::cout << "Instance a's Initiation Execution time: " << duration.count() << " micro s\n";
+        
+        start = std::chrono::high_resolution_clock::now();
+        School b{a};
+        end = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // std::cout << "Copying constructor a -> b Execution time: " << duration.count() << " micro s\n";
+        copy = duration.count();
+
+        start = std::chrono::high_resolution_clock::now();
+        School c{std::move(a)};
+        end = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // std::cout << "Moving constructor a -> c Execution time: " << duration.count() << " micro s\n";
+        move = duration.count();
+
+        copy_move_value[x] ={copy, move};
+
+        x -= 1;
+    }
+};
+
+
+int main(){
+
+    for(int T = 0; T < 4; T++){
+        tr.emplace_back(worker, T);
+    }
+
+    
+    for(auto& x : tr){
+        x.join();
+    }
+    
+    auto avg_copy = 0;
+    auto avg_move = 0;
+
+    for(auto each = copy_move_value.begin(); each != copy_move_value.end(); ++each){
+        // std::cout << "Copy:" << each->second[0] << "ns -> Move:" << each->second[1] << "ns" << std::endl;
+    
+        if (each == std::prev(copy_move_value.end())) break;
+
+        avg_copy += (int) each->second[0];
+        avg_move += (int) each->second[1];
+
+    }
+
+    avg_copy /= copy_move_value.size();
+    avg_move /= copy_move_value.size();
+
+    std::cout << "\nCopy VS Move constructor Execution time (Nanoseconds)\n";
+    std::cout << "Average Copy: " << avg_copy << "ns \nAverage Move: " << avg_move << "ns\n\n";
+
+    return 0;
+}
