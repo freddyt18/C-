a996db88a6fa46c7d4c9303327c493a2fd2597c7
freddyt18 <keopitou.doung@student.cadt.edu.kh>
2023-05-02 02:47:49 +0700

    Exercise 2, Section 1 of Chapter 2 of Book AV_C++

--- 2. Advanced C++/C2 - Move Semantics & Perfect Forwarding/Move Semantics/3_vector_move.cpp
+++ 2. Advanced C++/C2 - Move Semantics & Perfect Forwarding/Move Semantics/3_vector_move.cpp
@@ -6,37 +6,48 @@
 class Move_Vector {
     public:
         Move_Vector(std::string* ID){
+
+            this->DEFAULT += " of " + *ID;
             this->ID = ID;
+            
             std::cout << "Default Constructor with ID " << *ID << std::endl;
         }
 
         Move_Vector(Move_Vector&& other){
-            std::cout << "Moving Constructor with ID " << *ID << std::endl;
-
+            
             this->ID = other.ID;
-            this->ID = nullptr;
+            // this->DEFAULT = std::move(other.DEFAULT);
+
+            other.ID = nullptr;
+            
+            std::cout << "Moving Constructor with ID " << *ID << std::endl;
         }
 
         Move_Vector& operator=(Move_Vector&& other){
+            std::cout << "Moving Assignment Operator from ID " << *other.ID << " to ID " << *this->ID << std::endl;
+            
             if(this != &other){
-                delete[] ID;
+                delete ID;
 
                 ID = other.ID;
+                DEFAULT = other.DEFAULT;
+
                 other.ID = nullptr;
             }
 
-            std::cout << "Moving Assignment Operator" << std::endl;
 
             return *this;
         }
 
         ~Move_Vector(){
-            std::cout << "Destructor Called" << std::endl;
+            std::cout << "Destructor Called with ID " << ID << std::endl;
 
+            delete ID;
         }
         
         void display(){
-            std::cout << "ID: " << ID << std::endl;
+            std::cout << std::endl << "Default: " << this->DEFAULT << std::endl;
+            std::cout << "ID: " << *ID << std::endl << std::endl;
         }
     
     private:
@@ -46,10 +57,10 @@ class Move_Vector {
 
 
 int main(){
-    
+
     std::vector<std::string*> ID;
     std::vector<Move_Vector> Example;
-    
+
     for(int i = 0; i < 5; i++){
         ID.push_back(
             new std::string(
@@ -58,16 +69,31 @@ int main(){
         );
     }
 
-    for(auto each : ID){
-        std::cout << "Pushed " << each << std::endl;
-        Example.push_back(Move_Vector(each));
-        std::cout << "Done" << std::endl;
-    }
+    /* for(auto each : ID){
+        std::cout << "Pushing " << each << std::endl;
+        
+        // Pushing a new `Move_Vector` object into the `Example` vector, using a pointer to a string as
+        // e argument for the constructor. However, this can lead to unexpected behavior because the
+        // ove_Vector` class takes ownership of the string pointer and deletes it in its destructor.
+        // multiple `Move_Vector` objects are created with the same string pointer, they will all
+        // y to delete it, leading to undefined behavior.
+        Example.push_back(
+            Move_Vector{each}
+        );
 
-    Example[0] = std::move(Example[1]);
-    Example[2] = std::move(Example[3]);
+        std::cout << "Done" << std::endl << std::endl;
+    } */
 
-    Example[2].display();
+    Move_Vector a{ID[0]};
+    Move_Vector b{ID[1]};
+    Move_Vector c{ID[2]};
+    Move_Vector d{ID[3]};
+    Move_Vector e{ID[4]};
+
+    b = std::move(a);
+    c = std::move(d);
+
+    c.display();
 
     return 0;
 }
