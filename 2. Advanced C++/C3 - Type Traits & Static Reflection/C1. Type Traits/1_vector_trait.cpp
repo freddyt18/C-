9e26d4f5db1e467d4ef990535fd69904538f2f57
Freddy-git <dkpt123321@gmail.com>
2023-05-04 02:39:06 +0700

    Exercise 1, Subsection 1, Chapter 3 of Book AV_C++

--- 2. Advanced C++/C3 - Type Traits & Static Reflection/C1. Type Traits/1_vector_trait.cpp
+++ 2. Advanced C++/C3 - Type Traits & Static Reflection/C1. Type Traits/1_vector_trait.cpp
@@ -5,25 +5,26 @@
 
 using namespace std;
 
-
-template <typename T>
+// This function is used to check if the type is a vector or not but it is limited to only int and string
+template<typename T>
 struct is_vector_tt {
-
-    template <typename U>
-    static constexpr bool is_vector(U&& data) { 
-        return (std::is_same_v<U, std::vector<int>> || std::is_same_v<U, std::vector<std::string>>);
+    static constexpr bool is_vector(T&& data) {
+        return (std::is_same_v<T, std::vector<int>> || std::is_same_v<T, std::vector<std::string>>);
     }
 
-    static constexpr bool value = is_vector<T>(nullptr);
 };
+
 
 
 int main(){
 
-    static_assert(is_vector_tt<std::vector<int>>::value, "Not a vector");
-    static_assert(is_vector_tt<std::vector<std::string>>::value, "Not a vector");
-    static_assert(!is_vector_tt<int>::value, "Not a vector");
-    static_assert(!is_vector_tt<double>::value, "Not a vector");
+    cout << "v1 is vector - " << boolalpha << is_vector_tt<vector<int>>::is_vector(move(vector<int>{})) << endl;
+    cout << "v2 is vector - " << boolalpha << is_vector_tt<vector<string>>::is_vector(move(vector<string>{})) << endl;
+
+    // The result should be true but due to the limitation of the function, they will return false
+    cout << "v3 is vector - " << boolalpha << is_vector_tt<vector<double>>::is_vector(move(vector<double>{})) << endl;
+    cout << "v4 is vector - " << boolalpha << is_vector_tt<vector<bool>>::is_vector(move(vector<bool>{})) << endl;
+    cout << "v5 is vector - " << boolalpha << is_vector_tt<vector<vector<int>>>::is_vector(move(vector<vector<int>>{})) << endl;
 
 
     return 0;
